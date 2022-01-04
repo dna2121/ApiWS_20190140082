e83963b09a9f42e8ecb2b481881499211ae53880
dna2121 <dnatayawibisono@gmail.com>
2022-01-04 15:54:54 +0700

    remove unused

--- src/main/java/webservice/apiws/ws/jajanan/dataController.java
+++ src/main/java/webservice/apiws/ws/jajanan/dataController.java
@@ -6,16 +6,12 @@ package webservice.apiws.ws.jajanan;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import webservice.apiws.ws.jajanan.database.DataMhs;
